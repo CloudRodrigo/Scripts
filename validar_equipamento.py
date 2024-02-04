a86957f4ea4e19d64356313e1a40ad635b7a7033
JailsonCloudPark <157814284+JailsonCloudPark@users.noreply.github.com>
2024-02-04 19:08:42 -0300

    Update validar_equipamento.py

--- validar_equipamento.py
+++ validar_equipamento.py
@@ -3,11 +3,21 @@ import re
 
 
 
-CURRENT_CORRECT_VESION_CASHIER = '23.10.2'
-COMMAND_JARVIS_ENV= 'cat /etc/cloudpark/jarvis.env'
-COMMAND_VERSION_CASHIER='sudo dpkg -s cloudpark-desktop'
-COMANDO_JARVIS_IP='sudo jarvis ip'
-COMANDO_JARVIS_MACHINES='sudo jarvis machines'
+CURRENT_CORRECT_VERSION_CASHIER = '23.10.2'
+COMMAND_JARVIS_ENV = 'cat /etc/cloudpark/jarvis.env'
+COMMAND_VERSION_CASHIER = 'sudo dpkg -s cloudpark-desktop'
+COMMAND_JARVIS_IP = 'sudo jarvis ip'
+COMMAND_JARVIS_MACHINES = 'sudo jarvis machines'
+COMMAND_LS = 'ls'
+COMMAND_SWAP = 'free -h'
+COMMAND_CLEAR_SWAP = 'sudo swapoff -a ; sudo swapon -av'
+COMMAND_LOG_JARVIS = 'sudo tail -50 /var/log/cloudpark/jarvis.log'
+COMMAND_DATE = 'sudo date'
+COMMAND_JARVIS_STATUS = 'sudo service cloudpark-jarvis status'
+COMMAND_TEST_PING = 'ping -c 2 -w 2 google.com'
+
+
+
 LINK_UPPDATE_VERSION_CHASHIER= 'https://s3.sa-east-1.amazonaws.com/ferramentas.cloudpark.com.br/caixa/cloudpark-desktop_23.10.2_amd64.deb"'
 
 is_server:None
@@ -84,7 +94,7 @@ def exec_validation_version_cashier():
     try:
         result = subprocess.check_output(COMMAND_VERSION_CASHIER, shell=True).decode('utf-8')
         match = re.search(r'"Version":\s*"(\d+\.\d+\.\d+)"', result)
-        if match and match.group(1) != CURRENT_CORRECT_VESION_CASHIER:
+        if match and match.group(1) != CURRENT_CORRECT_VERSION_CASHIER:
             print(match.group(1))
             """update_version_cashier()"""
         else:
@@ -100,7 +110,7 @@ def update_version_cashier():
         subprocess.check_output(['wget', LINK_UPPDATE_VERSION_CHASHIER]).decode('utf-8')
         subprocess.check_output(['sudo', 'dpkg', '--purge', 'cloudpark-desktop']).decode('utf-8')
         subprocess.check_output(['sudo', 'apt-get', 'update'])
-        subprocess.check_output(['sudo', 'dpkg', '-i','cloudpark-desktop_'+CURRENT_CORRECT_VESION_CASHIER+'_amd64.deb'])
+        subprocess.check_output(['sudo', 'dpkg', '-i','cloudpark-desktop_'+CURRENT_CORRECT_VERSION_CASHIER+'_amd64.deb'])
         subprocess.check_output(['sudo', 'nano', '~/.config/autostart/cloudpark.desktop'])
         print("\n---------------CAIXA ATUALIZADO----------------\n")
         
@@ -108,7 +118,7 @@ def update_version_cashier():
         print(f"Erro inesperado: {e}")
 
 def print_info_ip():
-    result = subprocess.check_output(COMANDO_JARVIS_IP, shell=True).decode('utf-8')
+    result = subprocess.check_output(COMMAND_JARVIS_IP, shell=True).decode('utf-8')
     if "O IP está fixo" in result:
         print(f"O IP está fixo")
     else:
@@ -117,23 +127,23 @@ def print_info_ip():
 
 def print_jarvis_machines():
     print("\n----------------JARVIS MACHINES-_--------------\n")
-    result = subprocess.check_output(COMANDO_JARVIS_MACHINES, shell=True).decode('utf-8')
+    result = subprocess.check_output(COMMAND_JARVIS_MACHINES, shell=True).decode('utf-8')
     
     print(result)
 
 
 def print_date():
-    result = subprocess.check_output(['sudo', 'date']).decode('utf-8')
+    result = subprocess.check_output(COMMAND_DATE, shell=True).decode('utf-8')
     print("DATA= " + result)
 
 
 def print_has_share():
-    result = subprocess.check_output(['ls']).decode('utf-8')
+    result = subprocess.check_output(COMMAND_LS, shell=True).decode('utf-8')
     print("SHARE= EXISTE!! " if "share" in result else "SHARE= N/A")
 
 
 def exec_validation_swap():
-    result = subprocess.check_output(['free', '-h']).decode('utf-8')
+    result = subprocess.check_output(COMMAND_SWAP, shell=True).decode('utf-8')
     swap_line = [line for line in result.split('\n') if 'Swap' in line][0]
     used_swap = subprocess.check_output(['awk', '{print $3}', '/dev/stdin'], input=swap_line.encode('utf-8')).decode('utf-8').strip()
     if used_swap == '0B':
@@ -146,7 +156,7 @@ def clean_swap():
     try:
         print("\n-----------------LIMPANDO SWAP-----------------\n")
         
-        result = subprocess.check_output(['sudo', 'swapoff', '-a', ';', 'sudo', 'swapon', '-av']).decode('utf-8')
+        result = subprocess.check_output(COMMAND_CLEAR_SWAP, shell=True).decode('utf-8')
         print(result)
         print("-------------------TERMINOU--------------------\n")
         print("\n----------------------PI-----------------------\n")
@@ -159,7 +169,7 @@ def clean_swap():
 def print_log_jarvis():
     try:
         print("\n-----------------LOG JARVIS OK?-----------------\n")
-        result = subprocess.check_output(['sudo', 'tail', '-50', '/var/log/cloudpark/jarvis.log']).decode('utf-8')
+        result = subprocess.check_output(COMMAND_LOG_JARVIS, shell=True).decode('utf-8')
         print(result)
         print("-------------------TERMINOU--------------------")
         
@@ -169,15 +179,15 @@ def print_log_jarvis():
         print(f"Erro inesperado: {e}")
 
 def exec_jarvis_install():
-        result = subprocess.check_output(['sudo', 'service', 'cloudpark-jarvis', 'status']).decode('utf-8')
+        result = subprocess.check_output(COMMAND_JARVIS_STATUS, shell=True).decode('utf-8')
         if"running" in result:
             print("JARVIS INSTALADO")
             print_log_jarvis()
         else :
             print("JARVIS N/A")
             
-def priint_test_internet_connection():
-    result = subprocess.check_output(['ping','-c','2','-w', '2', 'google.com']).decode('utf-8')
+def print_test_internet_connection():
+    result = subprocess.check_output(COMMAND_TEST_PING, shell=True).decode('utf-8')
     if "PING google.com" in result:
          print("INTERNETC= CONECTADO")
     else:
@@ -191,15 +201,14 @@ def priint_test_internet_connection():
         print("SQLite= FALSE") """
 
 def main():
-    exec_cache_existence()
-    """  print_info_ip()
+    """ exec_cache_existence()
+    print_info_ip()
     print_date()
-    print_has_share()
+    print_has_share() """
     exec_validation_swap()
-    priint_test_internet_connection() 
-     print_exists_sqlite3()
+    print_test_internet_connection() 
     exec_jarvis_install()
-    print_jarvis_machines() """
+    print_jarvis_machines()
     for result in results_list:
         print(result)
     
